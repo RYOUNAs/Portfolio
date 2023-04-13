e82a93c88f6401d83b536aa6c15ba478281f86cd
RYOUNAs <101239369+RYOUNAs@users.noreply.github.com>
2023-04-14 05:44:31 +0900

    Add files via upload

---
+++ js/load.js
@@ -0,0 +1,5 @@
+//logoの表示
+$(window).on("load", function () {
+  $("#splash").delay(2500).fadeOut("slow"); //ローディング画面を2.5秒（2500ms）待機してからフェードアウト
+  $("#splash_logo").delay(2200).fadeOut("slow"); //ロゴを2.2秒（2200ms）待機してからフェードアウト
+});
