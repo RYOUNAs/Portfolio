fe8fcd79cd9f719ee496beffc3a9ae6aff37196d
RYOUNAs <101239369+RYOUNAs@users.noreply.github.com>
2023-04-19 20:44:24 +0900

    Add files via upload

--- js/Slide.js
+++ js/Slide.js
@@ -2,7 +2,7 @@ $(".slider").slick({
   fade: true, //切り替えをフェードで行う。初期値はfalse。
   autoplay: true, //自動的に動き出すか。初期値はfalse。
   autoplaySpeed: 6000, //次のスライドに切り替わる待ち時間
-  speed: 4000, //スライドの動きのスピード。初期値は300。
+  speed: 800, //スライドの動きのスピード。初期値は300。
   infinite: true, //スライドをループさせるかどうか。初期値はtrue。
   slidesToShow: 1, //スライドを画面に3枚見せる
   slidesToScroll: 1, //1回のスクロールで3枚の写真を移動して見せる
