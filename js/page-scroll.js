6301f65c7d2ca1f5ad93184f3e167a542e9136f4
RYOUNAs <101239369+RYOUNAs@users.noreply.github.com>
2023-04-21 14:21:02 +0900

    New

--- js/page-scroll.js
+++ js/page-scroll.js
@@ -1,4 +1,4 @@
-$('#page-link a[href*="#"]').click(function () {
+$('#menu a[href*="#"]').click(function () {
   //全てのページ内リンクに適用させたい場合はa[href*="#"]のみでもOK
   var elmHash = $(this).attr("href"); //ページ内リンクのHTMLタグhrefから、リンクされているエリアidの値を取得
   var pos = $(elmHash).offset().top; //idの上部の距離を取得
